865cf7409e0c3da1cab76e08987932dab9e9271f
allen.zhou <allen.zhou@xiaoi.com>
2020-03-09 13:59:21 +0800

    up

--- color_recognition/app.py
+++ color_recognition/app.py
@@ -21,20 +21,13 @@ def hello_world():
 @app.route('/colorDiscrimination', methods=['POST'])
 @jsonschema.validate('api', 'recognition')
 def recognition():
-    '''OA接口'''
+    """OA接口"""
     try:
         request_data = json.loads(request.data)
         app.logger.info("请求参数：{}".format(request_data))
         # 获取参数
-        url = request_data["url"]
-        # 判断文件是否存在
-        path = exists_file(url)
-        if path:
-            pass
-        else:
-            return jsonify(code=2, msg="文件不存在")
-
-    except Exception:
+        img = request_data["img"]
+    except Exception as e:
         traceback.print_exc()
         app.logger.exception(f"{request.data},异常：{traceback.print_exc()}")
         return jsonify(code=500, msg="内部错误")
