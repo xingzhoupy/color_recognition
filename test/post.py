4c1a66ef4be79f65099ed84d9e3b252206619836
allen.zhou <allen.zhou@xiaoi.com>
2020-03-25 09:17:20 +0800

    up

--- test/post.py
+++ test/post.py
@@ -10,8 +10,8 @@ import pprint
 
 if __name__ == '__main__':
     image_url = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1583742211078&di=e4ddad6dc96329fc07db1631ce7e77ad&imgtype=0&src=http%3A%2F%2Fa.vpimg4.com%2Fupload%2Fmerchandise%2F162065%2FQBH-B351716011-2.jpg"
-    url = "http://127.0.0.1:5050/colorDiscrimination"
-    # url = "http://222.85.230.14:12347/colorDiscrimination"
+    # url = "http://127.0.0.1:5050/colorDiscrimination"
+    url = "http://152.136.39.100:5000/colorDiscrimination"
     file_path = r"../test/1584067693(1).jpg"
     # image_url = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1583742211078&di=e4ddad6dc96329fc07db1631ce7e77ad&imgtype=0&src=http%3A%2F%2Fa.vpimg4.com%2Fupload%2Fmerchandise%2F162065%2FQBH-B351716011-2.jpg"
     # url = "http://127.0.0.1:5050/colorDiscrimination"
